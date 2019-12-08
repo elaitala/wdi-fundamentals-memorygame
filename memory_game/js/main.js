6a9b2f2c4dc0f56471557d298fcc33f4d512fdd0
Eric Laitala <elaitala@ErkkiPro.local>
2019-12-08 10:48:31 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,23 @@
+console.log("Up and running");
+
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
