c14fba75ec51a89a84ba46da8440ee20c207ed20
Eric Laitala <elaitala@ErkkiPro.local>
2019-12-08 18:09:30 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,7 +3,30 @@ console.log("Up and running");
 //console.log("User flipped " + cardOne);
 //console.log("User flipped " + cardThree);
 
-var cards = ["queen", "queen", "king", "king"];
+//var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 
@@ -17,10 +40,13 @@ var checkForMatch = function () {
   }
 }
 
-var flipCard = function(cardID) {
-	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+var flipCard = function(cardId) {
+	console.log('User flipped ' + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	cardsInPlay.push(cards[cardId].rank);
 }
 flipCard(0);
-flipCard(1);
+flipCard(2);
+checkForMatch();
